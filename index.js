aaf52ba7348c20b279827992e259f71e323f6dda
timskap <timskap@gmail.com>
2023-12-30 02:55:46 +0400

    add privacy

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const port = 8080;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
+
+app.get('/privacy', (req, res) => {
+    res.sendFile(__dirname + '/public/privacy.html');
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
